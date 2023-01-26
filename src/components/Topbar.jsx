cba608de9a3f6875afed53c1689fa8e381259b5d
oigoga <gogaelisabeth21@gmail.com>
2023-01-27 00:58:39 +0100

    debugging

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -6,19 +6,19 @@ const Topbar = () => {
   return (
     <>
       <div className="flex text-base font-semibold text-grey border-b-2 border-black py-8 ">
-              <NavLink to={'/deal'} className="text-grey border-b-4 w-1/3 mr-2" >
+              <NavLink to={'deal'} className="text-grey border-b-4 w-1/3 mr-2" >
               <button >
                 <div className="text-left">1. Deal  details</div>
               </button>
 
               </NavLink>
-              <NavLink to={'/tranche'} className="text-grey border-b-4 w-1/3 mx-2  ">
+              <NavLink to={'tranche'} className="text-grey border-b-4 w-1/3 mx-2  ">
               <button >
                 <div className="text-left">2. Tranche Structure</div>
               </button>
 
               </NavLink>
-              <NavLink to={'/review'} className="text-grey border-b-4 w-1/3 ml-2"  >
+              <NavLink to={'review'} className="text-grey border-b-4 w-1/3 ml-2"  >
               <button >
                 <div className="text-left">3. Review</div>
               </button>
